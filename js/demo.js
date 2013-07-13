acf16eec5d9e9f2c3d7150dbb0ba70e7f034f28d
Brice Fernandes <brice@fractallambda.com>
2013-07-13 22:10:03 +0100

    code cleanup and comment

--- js/demo.js
+++ js/demo.js
@@ -2,46 +2,51 @@
 
 function createNew(e){
   var newItem = $('<div>').addClass('item');
+
+  /*
+   * Create item
+   */ 
+  // Has a title.
   var title = $('<div>').addClass('title').text('Placeholder');
-  var connectNo = $('<div>').addClass('connect').addClass('connectNo');
-  var connectYes = $('<div>').addClass('connect').addClass('connectYes');
-
-  // The item is created where we click
-  newItem.css({'top': e.pageY,'left': e.pageX});
-
-  //The entire item is a target
-  jsPlumb.makeTarget(newItem, { anchor: 'Continuous' });
-
-  jsPlumb.draggable(newItem, {
-    containment: 'parent'
-  });
-  jsPlumb.makeSource(connectYes, {
-    parent: newItem,
-    anchor: 'Continuous'
-  });
-  jsPlumb.makeSource(connectNo, {
-    parent: newItem,
-    anchor: 'Continuous'
-  });
-
   newItem.append(title);
-  newItem.append(connectYes);
-  newItem.append(connectNo);
-
-
+  // is created where we click
+  newItem.css({'top': e.pageY,'left': e.pageX});
+  // is a target
+  jsPlumb.makeTarget(newItem, { anchor: 'Continuous' });
+  // it's draggable within its container
+  jsPlumb.draggable(newItem, { containment: 'parent' });
+  // Destroy on doubleclick.
   newItem.dblclick(function(e) {
     jsPlumb.detachAllConnections($(this));
     $(this).remove();
     e.stopPropagation();
   });
 
-  $('#container').append(newItem);
+  /*
+   * Create endpoints
+   */
+  // make the yes endpoint
+  var connectYes = $('<div>').addClass('connect').addClass('connectYes');
+  jsPlumb.makeSource(connectYes, {
+    parent: newItem,
+    anchor: 'Continuous'
+  });
+  newItem.append(connectYes);
 
+  // make the no endpoint 
+  var connectNo = $('<div>').addClass('connect').addClass('connectNo');
+  jsPlumb.makeSource(connectNo, {
+    parent: newItem,
+    anchor: 'Continuous'
+  });
+  newItem.append(connectNo);
+
+
+  // Add the new item to our container. 
+  // ideally this shouldn't be hard-coded, but recovered from somewhere.
+  $('#container').append(newItem);
 }
 
 jsPlumb.ready(function() {
-  
   $("#container").dblclick(createNew);
-  
-  
 });
