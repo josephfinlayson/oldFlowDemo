68ba80bfc372e18f50e8c9b0bbf7f55258e2f850
Brice Fernandes <brice@fractallambda.com>
2013-07-13 21:59:59 +0100

    Better styling for elements

--- js/demo.js
+++ js/demo.js
@@ -1,40 +1,47 @@
 
+
+function createNew(e){
+  var newItem = $('<div>').addClass('item');
+  var title = $('<div>').addClass('title').text('Placeholder');
+  var connectNo = $('<div>').addClass('connect').addClass('connectNo');
+  var connectYes = $('<div>').addClass('connect').addClass('connectYes');
+
+  // The item is created where we click
+  newItem.css({'top': e.pageY,'left': e.pageX});
+
+  //The entire item is a target
+  jsPlumb.makeTarget(newItem, { anchor: 'Continuous' });
+
+  jsPlumb.draggable(newItem, {
+    containment: 'parent'
+  });
+  jsPlumb.makeSource(connectYes, {
+    parent: newItem,
+    anchor: 'Continuous'
+  });
+  jsPlumb.makeSource(connectNo, {
+    parent: newItem,
+    anchor: 'Continuous'
+  });
+
+  newItem.append(title);
+  newItem.append(connectYes);
+  newItem.append(connectNo);
+
+
+  newItem.dblclick(function(e) {
+    jsPlumb.detachAllConnections($(this));
+    $(this).remove();
+    e.stopPropagation();
+  });
+
+  $('#container').append(newItem);
+
+}
+
 jsPlumb.ready(function() {
   
-
-  $("#container").dblclick(function(e){
-    var newState = $('<div>').addClass('item').text('Item n');
-    var title = $('<div>').addClass('title').text('State ');
-    var connect = $('<div>').addClass('connect');
-    
-    newState.css({
-      'top': e.pageY,
-      'left': e.pageX
-    });
-    
-    jsPlumb.makeTarget(newState, {
-      anchor: 'Continuous'
-    });
-    
-    jsPlumb.makeSource(connect, {
-      parent: newState,
-      anchor: 'Continuous'
-    });
-    
-    newState.append(title);
-    newState.append(connect);
-    
-    jsPlumb.draggable(newState, {
-      containment: 'parent'
-    });
- 
-    newState.dblclick(function(e) {
-      jsPlumb.detachAllConnections($(this));
-      $(this).remove();
-      e.stopPropagation();
-    });
-    
-    $('#container').append(newState);
-    
-  });
+  $("#container").dblclick(createNew);
+  
+  
 });
